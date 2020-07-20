d6f43880fa3aaafe08af27f19c3154375aa451bd
Juan Vásquez Castro <juanvascas@me.com>
2020-07-20 11:49:11 -0600

    using AppleALC-id=21, removing dependency of CodecCommander and JackFix, edit script to copy HiDPI files only

--- HiDPI_Files/install.sh
+++ HiDPI_Files/install.sh
@@ -9,82 +9,37 @@ echo "This Script Requires Administrator Privileges!"
 sudo mount -uw /
 
 C_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $C_DIR
-
-echo "Copying files to fix ALC256 combojack noise issue"
 
 function progressIndicator(){
 echo -ne '[..........................] (0%)\r'
-sleep 0.5
 echo -ne '[#.........................] (5%)\r'
-sleep 0.3
 echo -ne '[##........................] (10%)\r'
-sleep 0.3
 echo -ne '[###.......................] (15%)\r'
-sleep 0.5
 echo -ne '[####......................] (20%)\r'
-sleep 0.5
 echo -ne '[#####.....................] (25%)\r'
-sleep 0.3
 echo -ne '[#####.....................] (25%)\r'
-sleep 0.3
 echo -ne '[######....................] (30%)\r'
-sleep 0.3
 echo -ne '[#######...................] (35%)\r'
-sleep 0.3
 echo -ne '[########..................] (40%)\r'
-sleep 0.3
 echo -ne '[#########.................] (45%)\r'
-sleep 0.3
 echo -ne '[##########................] (50%)\r'
-sleep 0.3
 echo -ne '[###########...............] (55%)\r'
-sleep 0.3
 echo -ne '[[############..............] (60%)\r'
-sleep 0.3
 echo -ne '[#############.............] (65%)\r'
-sleep 0.3
 echo -ne '[###############...........] (70%)\r'
-sleep 0.3
 echo -ne '[#################.........] (75%)\r'
-sleep 0.3
 echo -ne '[####################......] (80%)\r'
-sleep 0.3
 echo -ne '[#######################...] (85%)\r'
-sleep 0.3
 echo -ne '[##########################] (100%)\r'
 echo -ne '\n'
 }
-
-sudo cp JackFix /usr/bin
-sudo chmod 755 /usr/bin/JackFix
-sudo chown root:wheel /usr/bin/JackFix
-echo "Install JackFix:"
-progressIndicator
-echo -ne "\n"
-
-
-sudo cp hda-verb /usr/bin
-sudo chmod 755 /usr/bin/hda-verb
-sudo chown root:wheel /usr/bin/hda-verb
-echo "Install hda-verb:"
-progressIndicator
-echo -ne "\n"
-
-sudo cp jack.fix.plist /Library/LaunchAgents/
-sudo chmod 644 /Library/LaunchAgents/jack.fix.plist
-sudo chown root:wheel /Library/LaunchAgents/jack.fix.plist
-sudo launchctl load /Library/LaunchAgents/jack.fix.plist
-echo "Install jack.fix.plist:"
-progressIndicator
-echo -ne "\n"
 
 cd /System/Library/Displays/Contents/Resources/Overrides/
 sudo mv Icons.plist IconsBackup.plist
 
 sudo mkdir DisplayVendorID-30e4
 
-cd $C_DIR/../HiDPI_Files/
+cd $C_DIR #navigate to path where install.sh was invoked
 
 echo "Copying files to enable native HiDPI resolutions"
 
