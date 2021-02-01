d0468d24e20ed5fc0fd1abde869d97d4cf62df30
Juan Vásquez Castro <juanvascas@me.com>
2021-02-01 09:08:03 -0600

    Update to OC 0.6.5 + kexts, ComboJack fix, TB3 ACPI by @kumakumalina

--- HiDPI_Files/install.sh
+++ HiDPI_Files/install.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-export mpoint=/System/Library/Displays/Contents/Resources/Overrides
-sudo mount -o nobrowse -t apfs /dev/disk"$1"s"$2" $mpoint
-sudo mkdir $mpoint/System/Library/Displays/Contents/Resources/Overrides/DisplayVendorID-30e4
-sudo mv $mpoint/System/Library/Displays/Contents/Resources/Overrides/Icons.plist $mpoint/System/Library/Displays/Contents/Resources/Overrides/IconsBackup.plist
-sudo cp Icons.plist $mpoint/System/Library/Displays/Contents/Resources/Overrides/
+echo "Enabling HiDPI"
+
+sudo mkdir -p /Library/Displays/Contents/Resources/Overrides/DisplayVendorID-30e4/
+sudo cp Icons.plist /Library/Displays/Contents/Resources/Overrides/
 cd DisplayVendorID-30e4
-sudo cp DisplayProductID-5d0 $mpoint/System/Library/Displays/Contents/Resources/Overrides/DisplayVendorID-30e4/
-sudo cp DisplayProductID-5d0.icns $mpoint/System/Library/Displays/Contents/Resources/Overrides/DisplayVendorID-30e4/
-sudo cp DisplayProductID-540 $mpoint/System/Library/Displays/Contents/Resources/Overrides/DisplayVendorID-30e4/
-sudo cp DisplayProductID-540.icns $mpoint/System/Library/Displays/Contents/Resources/Overrides/DisplayVendorID-30e4/
-sudo bless --folder $mpoint/ /System/Library/CoreServices --bootefi --create-snapshot
+sudo cp DisplayProductID-5d0 /Library/Displays/Contents/Resources/Overrides/DisplayVendorID-30e4/
+sudo cp DisplayProductID-5d0.icns /Library/Displays/Contents/Resources/Overrides/DisplayVendorID-30e4/
+sudo cp DisplayProductID-540 /Library/Displays/Contents/Resources/Overrides/DisplayVendorID-30e4/
+sudo cp DisplayProductID-540.icns /Library/Displays/Contents/Resources/Overrides/DisplayVendorID-30e4/
 
 echo "Done!"
 exit 0
